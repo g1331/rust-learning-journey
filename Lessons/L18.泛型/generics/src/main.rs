3da4c8e902dddeeaf5bc7b513e87e13904ad4e18
yc13 <caleb.1331@outlook.com>
2025-07-07 18:09:32 +0800

    feat(lessons): 添加泛型相关内容
    添加关于Rust泛型的示例代码和文档，包括函数、结构体和方法中的泛型使用，以及性能说明。

---
+++ Lessons/L18.泛型/generics/src/main.rs
@@ -0,0 +1,72 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// Rust无继承机制
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+
+    let integer = Point { x: 4, y: 4 };
+    let float = Point { x: 1.2, y: 1.2 };
+
+    println!("p,x = {}", integer.x());
+}
+
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
